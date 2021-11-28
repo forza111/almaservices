26c0006e548e71f0ff7a1e32a85b965b5ba52eeb
forza111 <nikitos.ionkin@yandex.ru>
2021-11-28 12:51:59 +0300

    change pipfile, route func, main.py

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ app.include_router(routers.router)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8002, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
 
--- routers.py
+++ routers.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter, BackgroundTasks
+from fastapi import APIRouter, BackgroundTasks, Depends
 from time import sleep
+from sqlalchemy.orm import Session
 
 import schemas
 from kernel import calc
+from database import get_db
+from crud import calculation
+
 
 router = APIRouter(
     prefix="/user",
@@ -10,10 +14,11 @@ router = APIRouter(
 )
 
 @router.post('/')
-async def get_user_repos(data: schemas.Data, background_tasks: BackgroundTasks):
-    date_start  = data.date_strt
-    date_fin = data.date_fin
-    lag = data.lag
-    background_tasks.add_task(calc, date_start,date_fin,lag)
+async def get_user_repos(data: schemas.Data, background_tasks: BackgroundTasks, db: Session = Depends(get_db)):
+    # date_start  = data.date_strt
+    # date_fin = data.date_fin
+    # lag = data.lag
+    # background_tasks.add_task(calc, date_start,date_fin,lag)
+    background_tasks.add_task(calculation.create_calc, db, data)
     return {"id": 1}
 
