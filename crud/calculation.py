7af30d17eb76a1872915a9a820e04148b6b8d647
forza111 <nikitos.ionkin@yandex.ru>
2021-11-29 09:49:26 +0300

    add CalcStatus to schemas.py add change_status_calc func to calculation.py

--- crud/calculation.py
+++ crud/calculation.py
@@ -6,11 +6,19 @@ from kernel import kern
 import database
 
 
-def create_calc(db: Session, data: schemas.CalcResult, id: schemas.CalcCreate):
+def change_status_calc(id: schemas.CalcCreate, db: Session = Depends(database.get_db)):
+    db_calc = db.query(models.Calculation).get(id)
+    db_calc.status = "Идет расчет"
+    db.add(db_calc)
+    db.commit()
+    db.refresh(db_calc)
+    return db_calc
+
+def create_calc(db: Session, data: schemas.CalcResult, id: schemas.CalcCreate, status_update: schemas.CalcStatus = Depends(change_status_calc(id))):
     res = kern(data.date_strt, data.date_fin, data.lag)
     db_calc = db.query(models.Calculation).get(id)
     db_calc.result = res
-    # db_calc = models.Calculation(result=res)
+    db_calc.status = "Завершен"
     db.add(db_calc)
     db.commit()
     db.refresh(db_calc)
@@ -23,3 +31,4 @@ def create_id_calc(db: Session = Depends(database.get_db)):
     db.commit()
     db.refresh(db_id_calc)
     return db_id_calc.id
+
--- schemas.py
+++ schemas.py
@@ -9,3 +9,6 @@ class CalcResult(BaseModel):
     date_strt: datetime.date
     date_fin: datetime.date
     lag: int
+
+class CalcStatus(BaseModel):
+    status: str
