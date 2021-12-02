b4270262c192c17dc001404f6ff0ef84789b488d
forza111 <nikitos.ionkin@yandex.ru>
2021-12-02 12:18:19 +0300

    add schemas validator

--- schemas.py
+++ schemas.py
@@ -1,8 +1,8 @@
 import datetime
 from datetime import timedelta
 
-from pydantic import BaseModel, Json
-
+from pydantic import BaseModel, Json, validator, ValidationError,root_validator
+from fastapi import HTTPException
 
 class CalcCreate(BaseModel):
     id: int
@@ -11,6 +11,17 @@ class CalcResult(BaseModel):
     date_strt: datetime.date
     date_fin: datetime.date
     lag: int
+
+    @root_validator
+    def check_correct_data(cls, v):
+        date_strt = v.get('date_strt')
+        date_fin = v.get('date_fin')
+        lag = v.get('lag')
+        if lag <= 0:
+            raise HTTPException(status_code=400, detail=f"field lag must be greater than zero, you passed {lag}")
+        if date_strt >= date_fin:
+            raise HTTPException(status_code=400, detail="Field date_fin must be larger than field date_strt")
+        return v
 
 class CalcList(BaseModel):
     calculation_name: str
